53a79eb975d3a1eee18a3cf10a895868802094d6
darbedar <darbedarab@gmail.com>
2025-02-04 10:02:46 +0330

    add lesson and student info structs

--- project.cpp
+++ project.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct lesson
+{
+    string lesson_name;
+    int unit;
+    float score;
+};
+struct student_info
+{
+    string name;
+    string major;
+    bool existance = false;
+    int id;
+    float gpa = 0;
+    lesson lesson_list[100];
+};
 int main()
 {
 }

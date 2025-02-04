69694c22ab74868a8eb44db4b1f736f8eb9d7a53
darbedar <darbedarab@gmail.com>
2025-02-04 10:27:35 +0330

    add lesson and student info structs

--- project.cpp
+++ project.cpp
@@ -15,6 +15,16 @@ struct student_info
     float gpa = 0;
     lesson lesson_list[100];
 };
+int mainmenu()
+{
+    int choice;
+    cout << "1. students" << endl
+         << "2. show all students" << endl
+         << "3. show students by major " << endl
+         << "4. exit programm" << endl;
+    cin >> choice;
+    return choice;
+}
 int main()
 {
 }

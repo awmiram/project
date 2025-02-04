9b007fa2342c2d0040cb826b9361f3b0c74f82ce
darbedar <darbedarab@gmail.com>
2025-02-04 12:23:36 +0330

    add gpa and updated structs

--- project.cpp
+++ project.cpp
@@ -5,6 +5,7 @@ struct lesson
     string lesson_name;
     int unit;
     float score;
+    bool existance = false;
 };
 struct student_info
 {
@@ -14,7 +15,13 @@ struct student_info
     int id;
     float gpa = 0;
     lesson lesson_list[100];
+    int unitsum = 0;
     void form();
+    void GPA();
+    void report_card();
+    void makeprofile();
+    void add_lesson();
+    void read_lesson(int);
 };
 void student_info::form()
 {
@@ -43,6 +50,47 @@ int search_student(student_info student[], int id)
             return i;
         }
     return -1;
+}
+void student_info::add_lesson()
+{
+    int choice;
+    for (int i = 0; i < 100; i++)
+    {
+        if (lesson_list[i].existance == true)
+            continue;
+        cout << "1. ADD LESSON " << endl
+             << "2.EXIT" << endl;
+        cin >> choice;
+        if (choice == 2)
+        {
+            break;
+        }
+        else if (choice != 1)
+        {
+            cout << "invalid please try again";
+            continue;
+        }
+        read_lesson(i);
+    }
+    GPA();
+}
+void student_info::GPA()
+{
+    int unitsum = 0;
+    gpa = 0;
+    for (int i = 0; i < 100 ; i++)
+    {
+        if (lesson_list[i].existance == false)
+            continue;
+        unitsum += lesson_list[i].unit;
+        gpa += lesson_list[i].unit * lesson_list[i].score;
+    }
+    gpa /= unitsum;
+}
+void student_info::makeprofile()
+{
+    form();
+    existance = true;
 }
 int mainmenu()
 {
@@ -123,7 +171,18 @@ lbl_menu:
                 }
                 case 2:
                 {
-                    
+                    switch(lesson_menu())
+                    {
+                        case 1:
+                        {
+                             int id;
+                            cout << "ENTER STUDENT ID : ";
+                            cin >> id;
+                            student[search_student(student, id)].add_lesson();
+                             goto lbl_menu;
+                            break;
+                        }
+                    }
                 }
             }
         }

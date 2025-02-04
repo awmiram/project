197567f679eab22129be965ae05d4d7fec5a16b4
awmiram <darbedarab@gmail.com>
2025-02-05 02:54:24 +0330

    last final touch

--- project.cpp
+++ project.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct lesson                                                          
+void getvalidint(auto &x)
 {
-    string lesson_name;                              
+    while (1)
+    {
+        cin >> x;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "INVALID INPUT! PLEASE TRY AGAIN" << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+struct lesson
+{
+    string lesson_name;
     int unit;
     float score;
     bool existance = false;
@@ -14,10 +31,10 @@ struct student_info
     bool existance = false;
     int id;
     float gpa = 0;
-    lesson lesson_list[100];
     int unitsum = 0;
     void form();
     void GPA();
+    lesson lesson_list[100];
     void report_card();
     void makeprofile();
     void add_lesson();
@@ -31,21 +48,19 @@ void student_info::form()
     cout << "ENTER MAJOR : " << endl;
     getline(cin, major);
     cout << "ENTER ID : " << endl;
-    cin >> id;
+    getvalidint(id);
     if (existance == false)
     {
         add_lesson();
     }
     GPA();
-
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     existance = true;
 }
 void student_info::GPA()
 {
     int unitsum = 0;
     gpa = 0;
-    for (int i = 0; i < 100 ; i++)
+    for (int i = 0; i < 100; i++)
     {
         if (lesson_list[i].existance == false)
             continue;
@@ -56,33 +71,52 @@ void student_info::GPA()
 }
 void student_info::read_lesson(int i)
 {
+    float x;
     cout << "ENTER LESSON NAME : ";
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, lesson_list[i].lesson_name);
     cout << "ENTER UNITS : ";
-    cin >> lesson_list[i].unit;
+    getvalidint(lesson_list[i].unit);
     cout << "ENTER SCORE : ";
-    cin >> lesson_list[i].score;
+    getvalidint(x);
+    lesson_list[i].score = x / 100.0;
     lesson_list[i].existance = true;
 }
 void student_info::report_card()
 {
-    if (existance == true)
+    if (!existance)
     {
-        cout << "NAME : " << name << endl
-             << "MAJOR : " << major << endl
-             << "ID : " << id << endl;
-        for (int i = 0; i < 100; i++)
-        {
-            if (lesson_list[i].existance == true)
-            {
-                cout << "LESSON : " << lesson_list[i].lesson_name << endl
-                     << "UNITS : " << lesson_list[i].unit << endl
-                     << "SCORE : " << lesson_list[i].score << endl;
-            }
-        }
-        cout << "STUDENT GPA : " << gpa << endl;
+        cout << "NO MATCH STUDENT!" << endl;
+        return;
     }
+
+    cout << "\n=====================================" << endl;
+    cout << "             REPORT CARD             " << endl;
+    cout << "=====================================" << endl;
+
+    cout << "NAME   : " << name << endl;
+    cout << "MAJOR  : " << major << endl;
+    cout << "ID     : " << id << endl;
+    cout << setfill('-') << setw(40) << "-" << setfill(' ') << endl;
+
+    cout << left << setw(20) << " LESSON"
+         << setw(10) << "| UNITS"
+         << setw(10) << "| SCORE" << endl;
+    cout << setfill('-') << setw(40) << "-" << setfill(' ') << endl;
+    for (int i = 0; i < 100; i++)
+    {
+        if (lesson_list[i].existance)
+        {
+            cout << left << setw(20) << lesson_list[i].lesson_name << "|"
+                 << setw(10) << lesson_list[i].unit << "|"
+                 << setw(9) << fixed << setprecision(2) << lesson_list[i].score << endl;
+        }
+    }
+    cout << setfill('-') << setw(40) << "-" << setfill(' ') << endl;
+
+    cout << "STUDENT GPA : " << fixed << setprecision(2) << gpa << endl;
+    cout << "=====================================" << endl
+         << endl;
 }
 void student_info::makeprofile()
 {
@@ -91,39 +125,51 @@ void student_info::makeprofile()
 }
 int mainmenu()
 {
+    cout << "======================================" << endl;
+    cout << "           MAIN MENU                " << endl;
+    cout << "======================================" << endl;
     int choice;
     cout << "1. students" << endl
          << "2. show all students" << endl
          << "3. show students by major " << endl
          << "4. exit programm" << endl;
-    cin >> choice;
+    getvalidint(choice);
     return choice;
 }
 int student_menu()
 {
+    cout << "======================================" << endl;
+    cout << "           STUDENT MENU                " << endl;
+    cout << "======================================" << endl;
     int choice;
     cout << "1.add student" << endl
          << "2. remove student" << endl
          << "3. edit student" << endl
          << "4. show student report card" << endl;
-    cin >> choice;
+    getvalidint(choice);
     return choice;
 }
 int edit_studentmenu()
 {
+    cout << "======================================" << endl;
+    cout << "           EDIT STUDENT               " << endl;
+    cout << "======================================" << endl;
     int choice;
     cout << "1. edit student personal info" << endl
          << "2. edit lessons" << endl;
-    cin >> choice;
+    getvalidint(choice);
     return choice;
 }
 int lesson_menu()
 {
+    cout << "======================================" << endl;
+    cout << "           LESSON MENU                " << endl;
+    cout << "======================================" << endl;
     int choice;
     cout << "1. add lesson" << endl
          << "2. remove lesson" << endl
          << "3. edit lesson" << endl;
-    cin >> choice;
+    getvalidint(choice);
     return choice;
 }
 void sortby_GPA(student_info student[])
@@ -185,7 +231,7 @@ void student_info::add_lesson()
             continue;
         cout << "1. ADD LESSON " << endl
              << "2.EXIT" << endl;
-        cin >> choice;
+        getvalidint(choice);
         if (choice == 2)
         {
             break;
@@ -209,7 +255,7 @@ void show_allstudent(student_info student[])
     {
         if (student[i].existance == true)
         {
-            cout << left << setw(6) << j<<'|'<< setw(29) << student[i].name<<'|'<< setw(19) << student[i].major<<'|'<< setw(14) << student[i].id<<'|'<< setw(5) << student[i].gpa<< endl;
+            cout << left << setw(6) << j << '|' << setw(29) << student[i].name << '|' << setw(19) << student[i].major << '|' << setw(14) << student[i].id << '|' << setw(5) << student[i].gpa << endl;
             j++;
             cout << setfill('-') << setw(80) << '-' << setfill(' ') << endl;
         }
@@ -219,7 +265,8 @@ void show_studentbymajor(student_info student[])
 {
     string major;
     cout << "ENTER MAJOR : " << endl;
-    cin >> major;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, major);
     sortby_GPA(student);
     cout << left << setw(6) << "RANK" << setw(30) << "| NAME" << setw(20) << "| MAJOR" << setw(15) << "| ID" << setw(5) << "| GPA" << endl;
     cout << setfill('-') << setw(80) << '-' << setfill(' ') << endl;
@@ -228,7 +275,7 @@ void show_studentbymajor(student_info student[])
     {
         if (student[i].existance == true && student[i].major == major)
         {
-            cout << left << setw(6) << j<<'|'<< setw(29) << student[i].name<<'|'<< setw(19) << student[i].major<<'|'<< setw(14) << student[i].id<<'|'<< setw(5) << student[i].gpa<< endl;
+            cout << left << setw(6) << j << '|' << setw(29) << student[i].name << '|' << setw(19) << student[i].major << '|' << setw(14) << student[i].id << '|' << setw(5) << student[i].gpa << endl;
             j++;
             cout << setfill('-') << setw(80) << '-' << setfill(' ') << endl;
         }
@@ -238,7 +285,7 @@ int main()
 {
     student_info student[20];
     int index;
-lbl_menu:
+lbl_mainmenu:
     switch (mainmenu())
     {
     case 1:
@@ -248,30 +295,46 @@ lbl_menu:
         case 1:
         {
             index = findfreespace(student);
+            if (index == -1)
+            {
+                cout << "NO SPACE FOR NEW STUDENT!" << endl;
+                goto lbl_mainmenu;
+            }
             student[index].form();
-            goto lbl_menu;
+            goto lbl_mainmenu;
             break;
         }
         case 2:
         {
             int id;
             cout << "ENTER SEARCH ID : " << endl;
-            cin >> id;
+            getvalidint(id);
+            if (search_student(student, id) == -1)
+            {
+                cout << "NO MATCH STUDENT!" << endl;
+                goto lbl_mainmenu;
+            }
             student[search_student(student, id)].existance = false;
-            goto lbl_menu;
+            goto lbl_mainmenu;
             break;
         }
         case 3:
         {
             switch (edit_studentmenu())
+            {
             case 1:
             {
                 int id;
                 cout << "ENTER STUDENT ID : ";
-                cin >> id;
+                getvalidint(id);
+                if (search_student(student, id) == -1)
+                {
+                    cout << "NO MATCH STUDENT!" << endl;
+                    goto lbl_mainmenu;
+                }
                 student[search_student(student, id)].form();
-                goto lbl_menu;
-                break;
+                goto lbl_mainmenu;
+                break;}
             case 2:
             {
                 switch (lesson_menu())
@@ -280,67 +343,105 @@ lbl_menu:
                 {
                     int id;
                     cout << "ENTER STUDENT ID : ";
-                    cin >> id;
+                    getvalidint(id);
+                    if (search_student(student, id) == -1)
+                    {
+                        cout << "NO MATCH STUDENT!" << endl;
+                        goto lbl_mainmenu;
+                    }
                     student[search_student(student, id)].add_lesson();
-                    goto lbl_menu;
+                    goto lbl_mainmenu;
                     break;
                 }
                 case 2:
                 {
                     int id;
                     cout << "ENTER STUDENT ID : " << endl;
-                    cin >> id;
+                    getvalidint(id);
+                    if (search_student(student, id) == -1)
+                    {
+                        cout << "NO MATCH STUDENT!" << endl;
+                        goto lbl_mainmenu;
+                    }
                     string name;
                     cout << "ENTER LESSON NAME : " << endl;
-                    cin >> name;
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    getline(cin, name);
+                    if (search_lesson(name, student, search_student(student, id)) == -1)
+                    {
+                        cout << "NO MATCH LESSON!" << endl;
+                        goto lbl_mainmenu;
+                    }
                     student[search_student(student, id)].lesson_list[search_lesson(name, student, search_student(student, id))].existance = false;
                     student[search_student(student, id)].GPA();
-                    goto lbl_menu;
+                    goto lbl_mainmenu;
                     break;
                 }
                 case 3:
                 {
                     int id;
                     cout << "ENTER STUDENT ID : " << endl;
-                    cin >> id;
+                    getvalidint(id);
                     string name;
                     cout << "ENTER LESSON NAME YOU WANT TO EDIT : " << endl;
-                    cin >> name;
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    getline(cin, name);
+                    if (search_lesson(name, student, search_student(student, id)) == -1)
+                    {
+                        cout << "NO MATCH LESSON!" << endl;
+                        goto lbl_mainmenu;
+                    }
                     student[search_student(student, id)].read_lesson(search_lesson(name, student, search_student(student, id)));
-                    goto lbl_menu;
+                    goto lbl_mainmenu;
                     break;
                 }
+                            default:
+                cout << "INVALID OPTION!, TRY AGAIN"<<endl;
+                goto lbl_mainmenu;
                 }
             }
+                        default:
+                cout << "INVALID OPTION!, TRY AGAIN"<<endl;
+                goto lbl_mainmenu;
             }
         }
         case 4:
         {
             int id;
             cout << "ENTER STUDENT ID :" << endl;
-            cin >> id;
+            getvalidint(id);
+            if (search_student(student, id) == -1)
+            {
+                cout << "NO MATCH STUDENT!" << endl;
+                goto lbl_mainmenu;
+            }
             student[search_student(student, id)].report_card();
-            goto lbl_menu;
+            goto lbl_mainmenu;
             break;
         }
+        default:
+            cout << "INVALID OPTION!, TRY AGAIN"<<endl;
         }
     }
 
     case 2:
     {
         show_allstudent(student);
-         goto lbl_menu;
-            break;
+        goto lbl_mainmenu;
+        break;
     }
     case 3:
     {
         show_studentbymajor(student);
-         goto lbl_menu;
-            break;
+        goto lbl_mainmenu;
+        break;
     }
     case 4:
     {
         return 0;
     }
-}
+    default:
+        cout << "INVALID OPTION!, TRY AGAIN"<<endl;
+        goto lbl_mainmenu;
+    }
 }
